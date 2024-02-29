4450a6492d6802f41c8f272e98960dc3366ac75c
David Wampamba <davidwampamba@gmail.com>
2024-02-29 16:17:47 +0300

    Refactor messageDetails

--- src/index.js
+++ src/index.js
@@ -3,38 +3,50 @@
 const { SendMailClient } = require("zeptomail");
 
 module.exports = {
-  provider: "zeptomail",
-  name: "Zeptomail",
+	provider: "zeptomail",
+	name: "Zeptomail",
 
-  init: function (providerOptions, settings) {
-    const mailClient = new SendMailClient({
-      url: providerOptions.url,
-      token: providerOptions.apiKey,
-    });
-    return {
-      send: async function (options) {
-        const { from, to, cc, bcc, replyTo, subject, text, html, ...rest } =
-          options;
-        const data = {
-          from: from || { address: settings.defaultFrom, name: "Insightify" },
-          to: [
-            {
-              email_address: {
-                address: to,
-              },
-            },
-          ],
-          subject,
-          htmlbody: html,
-          ...rest,
-        };
+	init: function (providerOptions, settings) {
+		const mailClient = new SendMailClient({
+			url: providerOptions.url,
+			token: providerOptions.apiKey,
+		});
+		return {
+			send: async function (options) {
+				const {
+					from,
+					to,
+					cc,
+					bcc,
+					replyTo,
+					subject,
+					text,
+					html,
+					...rest
+				} = options;
+				const messageDetails = {
+					from: {
+						address: from || settings.defaultFrom,
+						name: "Insightify",
+					},
+					to: [
+						{
+							email_address: {
+								address: to,
+							},
+						},
+					],
+					subject,
+					htmlbody: html || text,
+					...rest,
+				};
 
-        try {
-          return await mailClient.sendMail(data);
-        } catch (error) {
-          console.error(error);
-        }
-      },
-    };
-  },
+				try {
+					return await mailClient.sendMail(messageDetails);
+				} catch (error) {
+					console.error(error);
+				}
+			},
+		};
+	},
 };
